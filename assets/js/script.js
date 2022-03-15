1a6755f0bf32374567f1ae8038b777eda3c3f2bd
Strange-bs <80538114+Strange-bs@users.noreply.github.com>
2022-03-15 19:24:19 +0500

    the end for now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,186 +1,130 @@
-// :(
-
-  (function () {
-    const data = [
-      {
-        title: "Качественные материалы и возможность их выбора",
-        content:
-          "Vestibulum tincidunt nunc leo malesuada adipiscing ac non egestas nullam. At imperdiet cras eu nunc adipiscing lacus. Sed enim vel rhoncus nunc tellus. Bibendum integer quam massa duis lorem gravida.",
-      },
-      {
-        title: "title 2",
-        content: "content2",
-      },
-  
-      {
-        title: "title 3",
-        content: "content 3",
-      },
-  
-      {
-        title: "title 4",
-        content: "content 4",
-      },
-    ];
-    const easing = "easeOutExpo";
-    let target = document.querySelector("#data");
-    let tabs = document.querySelectorAll("#selectLayer .set");
-    for (let i = 0; i < tabs.length; i++) {
-      tabs[i].onclick = function () {
-        if (!tabs[i].className.includes("active")) {
-          for (let k = 0; k < tabs.length; k++) {
-            tabs[k].classList.remove("active");
-          }
-          let template = `<div class="title">
-                                          <span>${data[i].title}</span>
-                                      </div>
-                                      <div class="desc">
-                                          <span>${data[i].content}</span>
-                                      </div>`;
-          target.innerHTML = template;
-          anime({
-            targets: target.querySelector(".title"),
-            translateX: [-50, 0],
-            opacity: [0, 1],
-            easing: easing,
-            delay: anime.stagger(100),
-          });
-          anime({
-            targets: target.parentElement,
-            translateX: [50, 0],
-            opacity: [0, 1],
-            easing: easing,
-          });
-          anime({
-            targets: target.querySelector(".desc"),
-            translateX: [50, 0],
-            opacity: [0, 0.7],
-            easing: easing,
-          });
-          anime({
-            targets: target.parentElement.querySelector(".img"),
-            translateX: [50, 0],
-            easing: easing,
-          });
-          this.classList.add("active");
-        }
-      };
-    }
-  })();
-  (function(){
-    $scroll.onclick = e => {
-        (scroll(0, innerHeight ));
-        anime({
-            targets: $scroll,
-            opacity: [0, 1]
-        })
-    }
-  })();
-
-(function(){
-  const expectedScroll = 1330
-  window.onscroll = e => {
-    if (document.body.clientHeight - scrollY < expectedScroll){
-      anime({
-        targets: $back,
-        scale: 1,
-        opacity: 1
-      })
-    } else {
-      anime({
-        targets: $back,
-        opacity: 0,
-        scale: 0
-      })
-    }
-  }
-  $back.onclick = e => {
-    scroll(0, 0)
-  }
-})();
-
-(function(){
-  $toggler.onclick = e => {
-    $_hamburger.Toggle('show');
+const easing = "easeOutExpo";
+function changeTo(e, a) {
+  var t = document.querySelectorAll(".section-3 .container .col  .card"),
+    s = t[e];
+  t.forEach((e) => e.classList.remove("active")),
+    document
+      .querySelectorAll("#selectLayer .set")
+      .forEach((e) => e.classList.remove("active")),
+    a.classList.add("active"),
+    s.classList.add("active"),
     anime({
-      targets: $_hamburger.children[0].children,
+      targets: s.querySelector(".title"),
+      translateX: [-50, 0],
+      opacity: [0, 1],
+      easing: easing,
+      delay: anime.stagger(100),
+    }),
+    anime({
+      targets: s.parentElement,
       translateX: [50, 0],
       opacity: [0, 1],
-      delay: anime.stagger(100),
-      duration: 700,
-      easing: 'easeOutExpo'
-    })
-  }
-  
-})();
-
-(
-  function() {
-    function check(){
-        document.querySelectorAll('.splide').forEach(splide => {
-          if(window.innerWidth < 500) {
-            splide.css('display', 'block');
-            $ourProjects.css('display', 'none');
-            $banners.css('display', 'none');
-            $valunteer.css('display', 'none');
-          } else {
-            $valunteer.css('display', 'block');
-            splide.css('display', 'none');
-            $ourProjects.css('display', 'block');
-            $banners.css('display', 'block')
-          }
-        })
-    }
-    window.onresize = e => {
-      check()
-    }
-    check()
-  }
-)()
-
-;function openGateFor(someOne, tabTrigger) {
-  console.log(someOne)
-  var target = someOne || !1
-  const easing = "easeOutExpo" || ''
-  if (target || target == false){
-      var el = document.querySelectorAll('#planHouse .preview');
-      console.log(el[0])
-      el.forEach(e => e.classList.remove('active'))
-      document.querySelectorAll(".tabs .tab").forEach(tab => tab.classList.remove('active'))
-      tabTrigger.classList.add('active')
-      if(el.length) {
-          var current = el[someOne];
-          var info = current.querySelector(".info");
-          current.classList.add('active')
-          anime({
-            targets: current.querySelector(".preview .img"),
-            opacity: [0, 1],
-            translateX: [-300, 0],
-            easing: easing,
-          });
-          anime({
-            targets: info.querySelectorAll("p"),
-            opacity: [0, 1],
-            translateX: [-100, 0],
-            easing: easing,
-            delay: anime.stagger(100),
-          });
-          anime({
-            targets: info,
-            translateY: [-200, 0],
-            opacity: [0, 1],
-            easing: easing,
-          });
-          anime({
-            targets: info.querySelector(".title"),
-            opacity: [0, 1],
-            easing: easing,
-            translateX: [200, 0],
-          });
-      }
-  } 
+      easing: easing,
+    }),
+    anime({
+      targets: s.querySelector(".desc"),
+      translateX: [50, 0],
+      opacity: [0, 0.7],
+      easing: easing,
+    }),
+    anime({
+      targets: s.parentElement.querySelector(".img"),
+      translateX: [50, 0],
+      easing: easing,
+    });
 }
-
-document.querySelectorAll(".tabs .tab").forEach((a, b, c) => {
-  a.onclick = function() {openGateFor(b, this)}
-})
+function openGateFor(e, a) {
+  var t = e || !1;
+  if (t || 0 == t) {
+    var s = document.querySelectorAll("#planHouse .preview");
+    if (
+      (s.forEach((e) => e.classList.remove("active")),
+      document
+        .querySelectorAll(".tabs .tab")
+        .forEach((e) => e.classList.remove("active")),
+      a.classList.add("active"),
+      s.length)
+    ) {
+      var c = s[e],
+        n = c.querySelector(".info");
+      c.classList.add("active"),
+        anime({
+          targets: c.querySelector(".preview .img"),
+          opacity: [0, 1],
+          translateX: [-300, 0],
+          easing: easing,
+        }),
+        anime({
+          targets: n.querySelectorAll("p"),
+          opacity: [0, 1],
+          translateX: [-100, 0],
+          easing: easing,
+          delay: anime.stagger(100),
+        }),
+        anime({
+          targets: n,
+          translateY: [-200, 0],
+          opacity: [0, 1],
+          easing: easing,
+        }),
+        anime({
+          targets: n.querySelector(".title"),
+          opacity: [0, 1],
+          easing: easing,
+          translateX: [200, 0],
+        });
+    }
+  }
+}
+($scroll.onclick = (e) => {
+  scroll(0, innerHeight), anime({ targets: $scroll, opacity: [0, 1] });
+}),
+  (window.onscroll = (e) => {
+    document.body.clientHeight - scrollY < 1330
+      ? anime({ targets: $back, scale: 1, opacity: 1 })
+      : anime({ targets: $back, opacity: 0, scale: 0 });
+  }),
+  ($back.onclick = (e) => {
+    scroll(0, 0);
+  }),
+  ($toggler.onclick = (e) => {
+    $_hamburger.Toggle("show"),
+      anime({
+        targets: $_hamburger.children[0].children,
+        translateX: [50, 0],
+        opacity: [0, 1],
+        delay: anime.stagger(100),
+        duration: 700,
+        easing: "easeOutExpo",
+      });
+  }),
+  (function () {
+    function e() {
+      document.querySelectorAll(".splide").forEach((e) => {
+        window.innerWidth < 500
+          ? (e.css("display", "block"),
+            $ourProjects.css("display", "none"),
+            $banners.css("display", "none"),
+            $valunteer.css("display", "none"))
+          : ($valunteer.css("display", "block"),
+            e.css("display", "none"),
+            $ourProjects.css("display", "block"),
+            $banners.css("display", "block"));
+      });
+    }
+    (window.onresize = (a) => {
+      e();
+    }),
+      e();
+  })(),
+  document.querySelectorAll("#selectLayer .set").forEach(
+    (e, a) =>
+      (e.onclick = function () {
+        changeTo(a, this);
+      })
+  ),
+  document.querySelectorAll(".tabs .tab").forEach((e, a, t) => {
+    e.onclick = function () {
+      openGateFor(a, this);
+    };
+  });
